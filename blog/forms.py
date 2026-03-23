93907b3c8d539ff871370806a85f63c1d9e51295
Cezario <ikeaq.qq@mail.ru>
2026-03-23 07:12:39 +0300

    Добавлен красивый интерфейс, админка, обработка ошибок

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from .models import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Ваше имя'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Ваш email'}),
+            'body': forms.Textarea(attrs={'placeholder': 'Напишите комментарий...'}),
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.decorators.http import require_POST
+from .forms import CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+        )
 # Create your views here.
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post.published,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post, 'comments': comments, 'form': form}
+        )
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post.published, id=post_id)
+    comment = None
+
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
